c0ce1f8b9572b18300d2788f516be9756a0e68ef
João Gabriel <jg7607221@gmail.com>
2026-04-25 22:44:17 -0300

    Tem que resolver isso ae

---
+++ commands.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+
+def setup_commands(bot):
+    @bot.command(name="play")
+    
+    async def play(ctx):
+        if ctx.author.voice:
+            channel = ctx.author.voice.channel
+            await channel.connect()
+            await ctx.send(f"Conectado a {channel}! 🎶")
+        else:
+            await ctx.send("Você precisa estar em um canal de voz para usar este comando!")
+    
+    @bot.command(name="stop")
+    async def stop(ctx):
+        if ctx.voice_client:
+            await ctx.voice_client.disconnect()
+            await ctx.send("Desconectado! 🚪")
+        else:
+            await ctx.send("Eu não estou em nenhum canal de voz.")
+
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import discord
-import requests
 import os
-from discord.ext import commands, tasks
+from discord.ext import commands
 from dotenv import load_dotenv
 
+from commands import setup_commands
 
-Token = os.getenv('Token')
+# Carrega o Token
+load_dotenv()
+TOKEN = os.getenv('Token')
 
+if not TOKEN:
+    raise ValueError("Erro: Token não encontrado no arquivo .env")
+
+
+# Configuração do Bot
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command(name="hoje")
-async def mandar_agora(ctx):
-    await ctx.send("Envio manual processado! 🚀")
 
+# Registra os comandos
+setup_commands(bot)
 
-bot.run(Token)
+@bot.event
+async def on_ready():
+    print(f'Bot logado como \n ID:{bot.user}')
 
+if __name__ == "__main__":
+    bot.run(TOKEN)
