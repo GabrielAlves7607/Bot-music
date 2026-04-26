af2b34b66efa0b8ecba5db42b29a70cfd70e8325
João Gabriel <jg7607221@gmail.com>
2026-04-25 22:00:36 -0300

    bug detectado

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import discord
+import requests
+import os
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+
+
+Token = os.getenv('Token')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command(name="hoje")
+async def mandar_agora(ctx):
+    await ctx.send("Envio manual processado! 🚀")
+
+
+bot.run(Token)
 
